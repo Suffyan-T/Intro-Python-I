8712cb2dfb7fd748fd8dae31f6b59682d64b4e1f
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-06-02 09:39:35 -0700

    completed datatypes.py

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -8,14 +8,18 @@ changes, you'll get a TypeError saying you can't perform an operation
 on a string and an integer.
 """
 
-x = 5
-y = "7"
+x = 5   # x is an int
+y = "7" # y is a string
+
+#print(x+y)
+# Throws Error
 
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+print(x + int(y))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(str(x) + y)
